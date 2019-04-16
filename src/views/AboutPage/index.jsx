fb2c72c1221f43a32612447af4686ac505d156a8
unknown <tadeup1@gmail.com>
2019-04-16 00:11:14 -0300

    added tamplate for about page

--- src/views/AboutPage/index.jsx
+++ src/views/AboutPage/index.jsx
@@ -9,20 +9,33 @@ import { styles } from "./styles";
 import Paper from "@material-ui/core/es/Paper/Paper";
 import Typography from "@material-ui/core/es/Typography/Typography";
 
-class AboutPage extends Component {
-  state = {  };
+const AboutPage = (props) => {
+  const { classes } = props;
+  return (
+    <div className={classes.layout}>
+      <CssBaseline/>
+      <Typography component="h1" variant="h2">
+        Quem Somos
+      </Typography>
+      <br/>
 
-  render() {
-    return (
-      <div>
-        <CssBaseline/>
-        <Typography component="h1" variant="h5">
-          Quem Somos
-        </Typography>
-      </div>
-    );
-  }
-}
+      <Typography component='p' variant='h6'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod, justo ac eleifend sollicitudin, mi mauris aliquet justo, quis malesuada quam sem luctus nunc. Ut sollicitudin hendrerit lobortis. Sed sagittis, tortor in faucibus pretium, ipsum ante gravida erat, at vestibulum sem mi at nunc. Sed vel tellus urna. In hac habitasse platea dictumst. Suspendisse id nisl ac nibh blandit vulputate. Fusce et risus vehicula, vestibulum turpis eu, porttitor felis. Quisque ullamcorper semper diam a tincidunt. Nullam tempor dolor et tincidunt sollicitudin. Vestibulum a turpis vel neque posuere sodales. Duis placerat feugiat risus ut aliquam. Donec lectus urna, tristique ut lobortis nec, aliquam vel erat.
+      </Typography>
+      <br/>
+
+      <Typography component='p' variant='h6'>
+        In fringilla porttitor mauris. Nulla tristique iaculis suscipit. Quisque efficitur odio ipsum, non congue nisi posuere sit amet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce vehicula risus consequat, bibendum augue at, laoreet odio. Mauris maximus mollis urna id placerat. Aliquam interdum at ligula eu congue. Sed nec metus et augue rutrum ornare non nec sem. Suspendisse potenti.
+      </Typography>
+      <br/>
+
+      <Typography component='p' variant='h6'>
+        Praesent at elit vulputate, sodales justo eget, luctus enim. Pellentesque varius sollicitudin enim, tempor gravida ipsum viverra quis. Quisque nec pretium velit, vel vestibulum dui. Maecenas scelerisque, mi in efficitur vehicula, nulla diam ultrices nisl, vel eleifend arcu mauris in risus. Quisque nec sem sed enim scelerisque vulputate ut eu dui. Etiam id ligula libero. Morbi imperdiet odio molestie nulla auctor rhoncus blandit vitae justo. Curabitur iaculis, nibh ut hendrerit ullamcorper, sapien turpis tempor ipsum, et commodo lectus sapien a erat.
+      </Typography>
+
+    </div>
+  );
+};
 
 AboutPage.propTypes = {
 
--- src/views/AboutPage/styles.js
+++ src/views/AboutPage/styles.js
@@ -1,3 +1,12 @@
 export const styles = theme => ({
-
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(1300 + theme.spacing.unit * 3 * 2)]: {
+      width: 1200,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
 });
