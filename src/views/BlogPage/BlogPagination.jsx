5a04d43d38b7d4c5039df72ba0bb263bb2872fb8
unknown <tadeup1@gmail.com>
2019-04-17 22:36:56 -0300

    added pagination to posts page

--- src/views/BlogPage/BlogPagination.jsx
+++ src/views/BlogPage/BlogPagination.jsx
@@ -6,14 +6,111 @@ import { firestoreConnect, firebaseConnect } from 'react-redux-firebase'
 import CssBaseline from "@material-ui/core/es/CssBaseline/CssBaseline";
 import { withStyles } from "@material-ui/core";
 import { styles } from "./styles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import CardMedia from "@material-ui/core/es/CardMedia";
+import SearchField from "../../components/SearchField/SearchField";
+import Chip from "@material-ui/core/es/Chip";
+import Divider from "@material-ui/core/Divider";
+import PostElement from "../HomePage/PostsList/PostElement";
+import IconButton from "@material-ui/core/es/IconButton/IconButton";
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import {Link} from "react-router-dom";
+import Button from "@material-ui/core/es/Button";
+
+const mostRead = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas et massa leo',
+  'Nunc ullamcorper ultrices lacinia. Nulla a lacus pulvinar',
+  'Duis lacinia volutpat porta. Interdum et malesuada fames ac ante ipsum primis in faucibus.',
+  'Ut pharetra laoreet rutrum.',
+  'Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla aliquet lobortis est ac pulvinar.'
+];
 
 // STATELESS
 const BlogPagination = (props) => {
+  const renderPost = (post, key) => {
+    return (
+      <PostElement post={post} key={key}/>
+    )
+  };
+
+  const page = parseInt(props.match.params.page);
+
+  const BackLink = props => <Link to={`/blog/page/${page - 1}`} {...props}/>;
+  const NextLink = props => <Link to={`/blog/page/${page + 1}`} {...props}/>;
+
+  const posts = props.posts.map((post, key) => {
+    return key >= ((page-1)*10) && key <= ((page-1)*10+10)
+      ? renderPost(post, key)
+      : null
+  });
+  const { classes } = props;
+
   return (
-    <>
+    <React.Fragment>
       <CssBaseline/>
-      <p>{props.match.params.page}</p>
-    </>
+      <main className={classes.main}>
+        <Grid container spacing={16}>
+          <Grid item xs={8}>
+            <div className={classes.paper}>
+
+              <Typography variant="h6" gutterBottom style={{letterSpacing: 0.1 + 'em'}}>
+                POSTS
+              </Typography>
+              <Divider />
+              {posts}
+
+              <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+                className={classes.pageNavigator}
+              >
+                <Grid item>
+                  <IconButton
+                    component={BackLink}
+                    disabled={page <= 1}
+                    aria-label="Next Page"
+                  >
+                    <KeyboardArrowLeft />
+                  </IconButton>
+
+                  <Button color="primary" disabled>
+                    Página {page}
+                  </Button>
+
+                  <IconButton
+                    component={NextLink}
+                    disabled={posts.length % 10 > 0}
+                    aria-label="Next Page"
+                  >
+                    <KeyboardArrowRight />
+                  </IconButton>
+                </Grid>
+              </Grid>
+
+            </div>
+          </Grid>
+          <Grid item xs={4} className={classes.sidebarGrid}>
+            <div className={classes.sidebarContainer}>
+              <SearchField/>
+              <Typography component={'h3'} variant={'h5'}>
+                Mais Lidas
+              </Typography>
+              <ol>
+                {mostRead.map((el, key) => {
+                  return(
+                    <li key={key}>{el}</li>
+                  )
+                })}
+              </ol>
+            </div>
+          </Grid>
+        </Grid>
+      </main>
+    </React.Fragment>
   );
 };
 
@@ -23,7 +120,9 @@ BlogPagination.propTypes = {
 
 const mapStateToProps = state => {
   return {
-
+    posts: state.firestore.ordered.posts
+      ? state.firestore.ordered.posts
+      : [],
   }
 };
 
@@ -36,5 +135,13 @@ const mapDispatchToProps = dispatch => {
 export default compose(
   withStyles(styles),
   connect(mapStateToProps, mapDispatchToProps),
-  firestoreConnect(),
+  firestoreConnect((props) => {
+    return [
+      {
+        collection: 'posts',
+        orderBy: ['postDate', 'desc'],
+        limit: parseInt(props.match.params.page) * 10
+      }
+    ]
+  }),
 )(BlogPagination)
--- src/views/BlogPage/styles.js
+++ src/views/BlogPage/styles.js
@@ -9,4 +9,22 @@ export const styles = theme => ({
       marginRight: 'auto',
     },
   },
+
+  pageNavigator: {
+    marginTop: theme.spacing.unit * 3
+  },
+
+  paper: {
+    margin: `${theme.spacing.unit}px auto`,
+    padding: theme.spacing.unit * 2,
+  },
+
+  sidebarGrid: {
+    marginTop: theme.spacing.unit * 4
+  },
+
+  sidebarContainer: {
+    padding: '10px 0px 40px 20px',
+    borderLeft: '1px solid #e0e0e0',
+  }
 });
